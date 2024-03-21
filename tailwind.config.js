f7adf91337f18e73169cca59629198659db1cb74
Yvialga <lesailesvirevoltantes@proton.me>
2024-03-21 11:46:54 +0100

    fix location of custom color
    to not overwrite default color provide by tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,12 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    colors: {
-      primary: '#0A2252', //navy blue
-      secondary: '#F5C71D', //gold
+    extend: { //setting custom colors in extend, then in colors, without add colors {} after theme for not to overwrite the default palette.
+      colors: {
+        primary: '#0A2252', //navy blue
+        secondary: '#F5C71D', //gold
+      }
     },
-    extend: {},
   },
   plugins: [],
 }
