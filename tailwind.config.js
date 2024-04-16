7706a61664efb2b8ae1665cd563a72b753c93116
Yvialga <lesailesvirevoltantes@proton.me>
2024-04-16 10:52:10 +0200

    feat: add animations configuration for menu and overview

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,33 @@ module.exports = {
       colors: {
         primary: "rgb(var(--primary))", //navy blue
         secondary: 'rgb(var(--secondary))', //gold
+      },
+      textUnderlineOffset: {
+        5: '5px',
+      },
+      keyframes: {
+        scrollingText: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-50%)'}
+        },
+        'bounce-increase': {
+          '0%, 100%' : {
+            transform: "translateY(-70%)",
+            "animation-timing-function": "cubic-bezier(0.8, 0, 1, 1)"
+          },
+          '30%, 60%': {
+            opacity: 1
+          },
+          '50%': {
+            transform: "translateY(0)",
+            "animation-timing-function": "cubic-bezier(0, 0, 0.2, 1)",
+            opacity: 0
+          }
+        }
+      },
+      animation: {
+        scrollingText: 'scrollingText 45s infinite linear',
+        'bounce-increase': "bounce-increase 1s infinite"
       }
     },
   },
