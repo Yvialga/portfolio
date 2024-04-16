2e5694b86666996dda088fec16f7fcd07a4f727a
Yvialga <lesailesvirevoltantes@proton.me>
2024-04-16 10:53:37 +0200

    add overview at the top of the mainpage

---
+++ src/app/components/Overview/overview.jsx
@@ -0,0 +1,55 @@
+'use client'
+import Image from "next/image";
+import code_screen from "../../../assets/pictures/images/menu-code.png";
+
+
+const businessKeyWords = [
+    "web",
+    "full-stack",
+    "SEO",
+    "accessibilité",
+    "cyber-sécurité",
+    "javascript",
+    "react-js",
+    "tailwind-css",
+    "next-js",
+    "front-end",
+    "back-end"
+]
+
+export default function Overview() {
+    return (
+        <header className="text-white flex flex-col overflow-hidden ">
+            <Image 
+                src={code_screen}
+                alt="Capture d'écran de l'environnement de développement"
+                className="origin-top scale-150 md:transform-none md:w-2/4"
+            />
+            <div className="absolute flex flex-wrap flex-col items-center gap-y-4 mt-4 w-full">
+                <p className="font-bold mb-2.5 z-20">
+                    Je suis <em 
+                        className="not-italic capitalize  underline decoration-secondary underline-offset-5 decoration-4"
+                    >tommy <span className="uppercase">bernard</span></em>,
+                </p>
+                <h1 className="font-extrabold bg-primary p-1.5 w-max px-2.5 rounded-3xl z-20">Développeur web Full Stack</h1>
+                <p className="font-bold z-20">Développe des sites & applications mobiles</p>
+            </div>
+            <div className="scroll-indicator relative mb-4 w-6 h-12 border-2 border-white z-20 rounded-3xl self-center
+                            before:relative before:content-[''] before:animate-bounce-increase before:bg-white before:w-4 before:h-4
+                            before:inline-block before:rounded-full before:left-[2px] before:top-[20px]">
+            </div>
+            <ul className="flex gap-x-1.5 bg-primary w-max md:w-screen py-2.5 z-20 whitespace-nowrap animate-scrollingText md:animate-none md:justify-center">
+                {   // generate hashtags list
+                    [...Array(businessKeyWords.length)].map((e, i) => {
+                        return <li className="font-semibold">#{businessKeyWords[i]}</li>
+                    })
+                }
+                {   // generate second time hashtags to give an effect to text to disappear on the left to reappear on the right
+                    [...Array(businessKeyWords.length)].map((e, i) => {
+                        return <li className="font-semibold md:hidden">#{businessKeyWords[i]}</li>
+                    })
+                }
+            </ul>
+        </header>
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
 import "../assets/styles/globals.css";
 import NavBar from "./components/NavBar/navbar";
+import Overview from "./components/Overview/overview"
 
 export default function Home() {
   return (
+    <>
       <NavBar />
+      <Overview />
+    </>
   );
 }
