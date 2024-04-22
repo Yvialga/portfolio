53e5aab59e4132dbaa2d2cabc820e34033883d22
Yvialga <lesailesvirevoltantes@proton.me>
2024-04-22 10:21:54 +0200

    edit layout of overview apperance

--- src/app/components/Overview/overview.jsx
+++ src/app/components/Overview/overview.jsx
@@ -2,7 +2,6 @@
 import Image from "next/image";
 import code_screen from "../../../assets/pictures/images/menu-code.png";
 
-
 const businessKeyWords = [
     "web",
     "full-stack",
@@ -25,14 +24,14 @@ export default function Overview() {
                 alt="Capture d'écran de l'environnement de développement"
                 className="origin-top scale-150 md:transform-none md:w-2/4"
             />
-            <div className="absolute flex flex-wrap flex-col items-center gap-y-4 mt-4 w-full">
+            <div className="absolute flex flex-wrap flex-col items-center gap-y-4 sm:gap-y-6 mt-4 sm:mt-20 w-full">
                 <p className="font-bold mb-2.5 z-20">
                     Je suis <em 
                         className="not-italic capitalize  underline decoration-secondary underline-offset-5 decoration-4"
                     >tommy <span className="uppercase">bernard</span></em>,
                 </p>
                 <h1 className="font-extrabold bg-primary p-1.5 w-max px-2.5 rounded-3xl z-20">Développeur web Full Stack</h1>
-                <p className="font-bold z-20">Développe des sites & applications mobiles</p>
+                <p className="font-bold z-20 text-center">Développe des sites web visibles sur ordinateurs, tablettes, et téléphones</p>
             </div>
             <div className="scroll-indicator relative mb-4 w-6 h-12 border-2 border-white z-20 rounded-3xl self-center
                             before:relative before:content-[''] before:animate-bounce-increase before:bg-white before:w-4 before:h-4
