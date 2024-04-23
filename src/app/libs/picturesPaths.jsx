eac13d0d03c150aff907e598b5f12d2a7b27f459
Yvialga <lesailesvirevoltantes@proton.me>
2024-04-23 11:32:42 +0200

    feat: function to get the right logo

--- src/app/libs/picturesPaths.jsx
+++ src/app/libs/picturesPaths.jsx
@@ -1,9 +1,12 @@
+import Image from "next/image";
+
 import clipboard from "../../assets/pictures/icons/clipboard-icon.png";
 import chat from "../../assets/pictures/icons/dark-chat-icon.png";
 import diamond from "../../assets/pictures/icons/diamond-icon.png";
 import mail from "../../assets/pictures/icons/mail-icon.png";
 import upArrow from "../../assets/pictures/icons/up-arrow-icon.png";
 
+import js from "../../assets/pictures/logos/js-logo.png";
 import illustrator from "../../assets/pictures/logos/ai_icon.png";
 import css from "../../assets/pictures/logos/css3-logo.png";
 import eclipse from "../../assets/pictures/logos/eclipse-logo.png";
@@ -17,3 +20,76 @@ import php from "../../assets/pictures/logos/php-logo.png";
 import reactjs from "../../assets/pictures/logos/reactjs-logo.png";
 import sass from "../../assets/pictures/logos/sass-logo.png";
 import vscode from "../../assets/pictures/logos/vscode-logo.png";
+import jira from "../../assets/pictures/logos/jira-logo.png";
+import sql from "../../assets/pictures/logos/sql-logo.png";
+import tailwind from "../../assets/pictures/logos/tailwindcss-mark.svg";
+
+/**Return the next component 'image' with path that point to the right path.
+ * 
+ * @param {Object} describingText
+ * @param {String} describingText.toolName Tool name to return the right image.
+ * @returns {import("react").ReactElement}
+ */
+export function ChooseTheRightImage({toolName}) {
+    let formattedTextToTest = toolName.toLowerCase().replaceAll(".", "");
+    let imagePath;
+
+    switch (formattedTextToTest) {
+        case "html":
+            imagePath = html;
+            break;
+        case "css":
+            imagePath = css;
+            break;
+        case "javascript":
+            imagePath = js; 
+            break;
+        case "scss":
+            imagePath = sass;
+            break;
+        case "php":
+            imagePath = php;
+            break;
+        case "java":
+            imagePath = java;
+            break;
+        case "sql":
+            imagePath = sql;
+            break;
+    // Frameworks part
+        case "reactjs":
+            imagePath = reactjs;
+            break;
+        case "nextjs":
+            imagePath = nextjs;
+            break;
+        case "tailwind css":
+            imagePath = tailwind;
+            break;
+    // Tools part
+            case "vs code":
+            imagePath = vscode;
+            break;
+            case "github":
+            imagePath = gitHub;
+            break;
+            case "figma":
+            imagePath = figma;
+            break;
+            case "illustrator":
+            imagePath = illustrator;
+            break;
+            case "ide eclipse":
+            imagePath = eclipse;
+            break;
+            case "jira":
+            imagePath = jira;
+            break;
+        
+        default:
+            imagePath = html;
+            break;
+    }
+    
+    return <Image src={imagePath} alt={`Logo de ${toolName}`} className="w-8 h-8" width={35} height={35}/>
+}
