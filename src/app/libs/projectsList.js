abe1a629981e2932eaf54fb759d817b141f3118b
Yvialga <lesailesvirevoltantes@proton.me>
2024-04-30 14:20:01 +0200

    adapt projects const for their screen

--- src/app/libs/projectsList.js
+++ src/app/libs/projectsList.js
@@ -1,11 +1,13 @@
-import Image from "next/image";
+import siteBookiScreen from "../../assets/pictures/images/screen_site_booki.png";
 
 export const PROJECTS_LIST = [
     {
         title: 'booki',
-        desc: "Site fictif de recherche d'hébergement"
+        desc: "Site fictif de recherche d'hébergement",
+        src: siteBookiScreen
     }, {
         title: 'portfolio',
-        desc: "Site portfolio"
+        desc: "Site portfolio",
+        src: siteBookiScreen // TODO
     }
 ];
