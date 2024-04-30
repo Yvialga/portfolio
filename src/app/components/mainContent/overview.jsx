0f0fac1f0f348270861a81a3d03db81f353a2fce
Yvialga <lesailesvirevoltantes@proton.me>
2024-04-30 20:17:47 +0200

    fix scroll indicator no visible in desktop view

--- src/app/components/mainContent/overview.jsx
+++ src/app/components/mainContent/overview.jsx
@@ -18,8 +18,8 @@ const businessKeyWords = [
 
 export default function Overview() {
     return (
-        <header className="text-white bg-white flex flex-col md:flex-wrap items-start md:items-center overflow-hidden relative left-1/2 transform -translate-x-1/2
-                            w-full md:w-[50%] 2md:w-[40%] md:my-12 md:rounded-[3rem] md:shadow-[0_0_20px_10px_rgb(0,0,0,0.1)]"> 
+        <header className="text-white bg-white flex flex-col md:flex-wrap items-start md:items-center overflow-hidden md:overflow-visible relative left-1/2 transform -translate-x-1/2
+                            w-full md:w-[50%] md2:w-[40%] md:my-12 md:rounded-[3rem] md:shadow-3xl"> 
             <Image 
                 src={code_screen}
                 alt="Capture d'écran de l'environnement de développement"
@@ -34,12 +34,12 @@ export default function Overview() {
                 <h1 className="font-extrabold bg-primary p-1.5 w-max px-2.5 rounded-3xl z-20">Développeur web Full Stack</h1>
                 <p className="font-bold z-20 text-center md:text-black px-2">Développe des sites web pour ordinateurs, tablettes, et téléphones</p>
             </div>
-            <div className="scroll-indicator relative md:absolute md:translate-y-[700%] mb-4 w-6 h-12 border-2 border-white z-20 rounded-3xl self-center
-                            before:relative before:content-[''] before:animate-bounce-increase before:bg-white  before:w-4 before:h-4
+            <div className="scroll-indicator relative md:absolute md:transform md:translate-y-[650%] lg:translate-y-[580%] mb-4 w-6 h-12 border-2 border-white z-50 rounded-3xl self-center
+                            before:relative before:content-[''] before:animate-bounce-increase before:bg-white before:z-50 before:w-4 before:h-4
                             before:inline-block before:rounded-full before:left-[2px] before:top-[20px] md:left-1/2">
             </div>
             <ul className="flex md:flex-wrap gap-x-1.5 bg-primary w-max py-2.5 md:px-1 z-20 md:w-full whitespace-nowrap
-                            animate-scrollingText md:animate-none md:justify-center">
+                            animate-scrollingText md:animate-none md:justify-center md:rounded-b-[2.5rem]">
                 {   // generate hashtags list
                     [...Array(businessKeyWords.length)].map((e, i) => {
                         return <li className="font-semibold">#{businessKeyWords[i]}</li>
