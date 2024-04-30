5be8c60419f79c30f51e7c0951ea50f018b464b1
Yvialga <lesailesvirevoltantes@proton.me>
2024-04-30 20:25:54 +0200

    style: make separator usable in various case

--- src/app/libs/contentSeparatorBar.jsx
+++ src/app/libs/contentSeparatorBar.jsx
@@ -1,7 +1,15 @@
-export default function ContentSeparatorBar({iteration, arraySize, handleDisplayBar}) {
+
+/**
+ * 
+ * @param {Object} specifications 
+ * @param {Number} specifications.iteration An Integer which represents the current iteration
+ * @param {Number} specifications.arraySize Size of array. Usually pass with length property
+ * @returns 
+ */
+export default function ContentSeparatorBar({iteration, arraySize}) {
     return <>
         {
-            ((++iteration) < arraySize) ? <hr className={`rotate-90 transform translate-y-2 border-none bg-secondary h-1 w-10 my-8 text-center ${handleDisplayBar ? "hidden" : "block"}`} />
+            ((++iteration) < arraySize) ? <hr className={`rotate-90 transform translate-y-2 border-none bg-secondary h-1 w-10 my-8 text-center`} />
                 : <hr className="hidden" />
         }
     </>
