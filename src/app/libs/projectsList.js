b7ccfbc623ef47717ffa04eee87cb98227d0ec30
Yvialga <lesailesvirevoltantes@proton.me>
2024-05-02 10:20:12 +0200

    add image for portfolio project card

--- src/app/libs/projectsList.js
+++ src/app/libs/projectsList.js
@@ -1,13 +1,14 @@
-import siteBookiScreen from "../../assets/pictures/images/screen_site_booki.png";
+import BookiSiteScreen from "../../assets/pictures/images/screen_site_booki.png";
+import portfolioSiteScreeshot from "../../assets/pictures/images/screenshot_site_portfolio.png";
 
 export const PROJECTS_LIST = [
     {
         title: 'booki',
         desc: "Site fictif de recherche d'hébergement",
-        src: siteBookiScreen
+        src: BookiSiteScreen
     }, {
         title: 'portfolio',
         desc: "Site portfolio",
-        src: siteBookiScreen // TODO
+        src: portfolioSiteScreeshot
     }
 ];
